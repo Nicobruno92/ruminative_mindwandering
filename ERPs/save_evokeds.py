e4e60dd16818cd06347acf4bdf633425f8fcccab
nicobruno92@gmail.com <nicobruno92@gmail.com>
2024-11-07 13:19:16 +0100

    pickle results evokeds

--- ERPs/save_evokeds.py
+++ ERPs/save_evokeds.py
@@ -16,8 +16,8 @@ from ERPs.generate_evokeds import process_subjects_parallel
 print('Packages loaded')
 
 # Paths and settings
-# root = "/network/lustre/iss02/cenir/analyse/meeg/CYBERSART/"
-root = "//l2export/iss02.cenimodule r/analyse/meeg/CYBERSART/"
+root = "/network/lustre/iss02/cenir/analyse/meeg/CYBERSART/"
+# root = "//l2export/iss02.cenimodule r/analyse/meeg/CYBERSART/"
 
 derivatives_folder = os.path.join(root, "derivatives_nico")
 subjects = [f"{i:02}" for i in range(2, 43)]
@@ -33,14 +33,8 @@ offtask_metrics = ['mean', 'median', 'quartiles', 'tertiles', 'highlow']
 
 # List of electrodes of interest (ROI)
 posterior_roi = ['C3', 'Cz', 'C4', 'P3', 'Pz', 'P4']
-
-#%%
-# Process subjects and save evokeds for each metric
-print("Processing subjects and generating evokeds...")
-process_subjects_parallel(root, subjects, tasks, metrics=offtask_metrics, data=data, ref_channels=['TP9', 'TP10'], distance=5, n_jobs=-1)
-
-#%%
 # Compute grand averages and plot ERPs
+
 for metric in offtask_metrics:
     print(f"Computing grand averages for metric: {metric}")
 
@@ -58,13 +52,3 @@ for metric in offtask_metrics:
     with open(output_file_path, 'wb') as file:
         pickle.dump(participant_evokeds, file)
         print(f"participant_evokeds object saved to {output_file_path}")
-    
-    # Plot and save ERP figures for each metric
-    fig = plot_erp(participant_evokeds, conditions_of_interest, posterior_roi, return_fig=True)
-    # fig.write_image(f"./results/ERPs/figs/ERP_{metric}_mastoid.png")
-    fig.write_html(f"./results/ERPs/figs/ERP_{metric}_car.html")
-    print(f"ERP figure saved for metric {metric}.")
-
-print("Processing completed.")
-
-# %%
